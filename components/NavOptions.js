04de89c827bdf614fc803eb4d957e16d8f2c6e7b
SaiyedRushan <rushan52@gmail.com>
2024-02-29 17:25:52 -0500

    navigate to screen on press

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -18,13 +19,15 @@ const data = [
   },
 ];
 const NavOptions = () => {
+  const navigation = useNavigation()
+
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({item}) => 
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} onPress={()=> navigation.navigate(item.screen)}>
           <View>
             <Image source={{ uri: item.image }} style={{ width: 120, height: 120, resizeMode: "contain" }} />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
