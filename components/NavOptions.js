d4c19634215dd23f0a505b58e3042b4441555b74
SaiyedRushan <rushan52@gmail.com>
2024-02-29 17:51:00 -0500

    add googleplacesautocomplete component

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -19,23 +19,22 @@ const data = [
   },
 ];
 const NavOptions = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
-      renderItem={({item}) => 
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} onPress={()=> navigation.navigate(item.screen)}>
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} onPress={() => navigation.navigate(item.screen)}>
           <View>
             <Image source={{ uri: item.image }} style={{ width: 120, height: 120, resizeMode: "contain" }} />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-            <Icon name="arrowright" color="white" type="antdesign" style={tw`p-2 bg-black rounded-full w-10 mt-4`}/>
+            <Icon name="arrowright" color="white" type="antdesign" style={tw`p-2 bg-black rounded-full w-10 mt-4`} />
           </View>
         </TouchableOpacity>
-      }
-
+      )}
     />
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,56 @@
-import { Text, SafeAreaView, Image, View } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
-import NavOptions from '../components/NavOptions'
+import { Text, SafeAreaView, Image, View } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { Input } from "react-native-elements";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
-        <Image
-          style={{ width: 100, height: 100, resizeMode: 'contain' }}
-          source={{ uri: 'https://links.papareact.com/gzs' }}
+        <Image style={{ width: 100, height: 100, resizeMode: "contain" }} source={{ uri: "https://links.papareact.com/gzs" }} />
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+              padding: 4,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          minLength={2}
+          enablePoweredByContainer={false}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+            components: "country:ca",
+          }}
         />
         <NavOptions />
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
