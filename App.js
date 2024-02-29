3140a90d691597ccb38ac2b6c4f14f2d52d77dd9
SaiyedRushan <rushan52@gmail.com>
2024-02-29 18:57:11 -0500

    add keyboardavoidingview

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
-import { Provider } from "react-redux";
-import { store } from "./store";
-import HomeScreen from "./screens/HomeScreen";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MapScreen from "./screens/MapScreen";
+import { Provider } from 'react-redux'
+import { store } from './store'
+import HomeScreen from './screens/HomeScreen'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import MapScreen from './screens/MapScreen'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator initialRouteName="HomeScreen">
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false}}/>
-          </Stack.Navigator>
+          <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS == 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator initialRouteName="HomeScreen">
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
-  );
+  )
 }
