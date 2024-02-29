003efb4602b88766434645d963cf98a11f166259
SaiyedRushan <rushan52@gmail.com>
2024-02-29 17:07:46 -0500

    create redux store with navslice and create home page

---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import { Text, SafeAreaView, Image, View } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+import NavOptions from '../components/NavOptions'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          source={{ uri: 'https://links.papareact.com/gzs' }}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
